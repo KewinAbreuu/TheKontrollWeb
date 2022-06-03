565a2630b209b6ee3cab4137b5fdca17953a5b74
kewin_Abreu <87028430+KewinAbreuu@users.noreply.github.com>
2022-06-03 11:51:08 -0300

    [KewinAbreu] - Modificação Cards Ocorencias

--- src/components/CardsOcorrencia/index.js
+++ src/components/CardsOcorrencia/index.js
@@ -6,11 +6,11 @@ import firebase from '../../services/firebaseConect'
  export default function CardsOcorrencia({local, data, hora, id,desc,funcionario,titulo}) {
 
 
-  async function deletar(){
-    await firebase.firestore().collection('ocorrencias')
-    .doc(id)
-    .delete();
-  }
+  // async function deletar(){
+  //   await firebase.firestore().collection('ocorrencias')
+  //   .doc(id)
+  //   .delete();
+  // }
 
   async function Ver(){
     await firebase.firestore().collection('ocorrencias')
@@ -20,21 +20,21 @@ import firebase from '../../services/firebaseConect'
   
     return (
       <>
-      <div className="card-container2" id='card-container' onClick={Ver}>
+      <div className="card-container2" id='card-container'>
         
         <div className="card2" id='card'>
 
             <div className="card-info2" id='card-info' >
                 <p className='pa2'>{titulo}</p>
-                <button onClick={deletar} className='BtnDelete'>Delete</button>
-                {/* <button onClick={Ver} className='BtnDelete'>Ver</button> */}
             </div>
 
             <div className="card-info22" id='card-info2'>
-                {/* <p className='pa2'>{data}</p>  */}
+                <p className='pa2'>{data}</p> 
             </div>
 
-            <div className='Barralateral'></div>
+            {/* <div className='Barralateral'></div> */}
+            <button onClick={Ver} className='BtnDelete'>Ver</button>
+
             
         </div>
          
